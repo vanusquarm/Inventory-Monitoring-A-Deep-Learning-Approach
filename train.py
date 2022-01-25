8f2b922dac7330f4fd8b316362dadd4f1559d7a1
sylroyalle <35784409+vanusquarm@users.noreply.github.com>
2022-01-25 11:35:43 +0000

    Create train.py

---
+++ train.py
@@ -0,0 +1,177 @@
+
+import sys
+from tqdm import tqdm
+from PIL import ImageFile
+import copy
+import argparse
+import os
+import logging
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torchvision import datasets
+
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+import smdebug.pytorch as smd
+
+
+
+
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def test(model, test_loader, criterion , hook):
+    model.eval()
+    hook.set_mode(smd.modes.EVAL)
+    running_loss=0
+    running_corrects=0
+    
+    for inputs, labels in test_loader:
+        outputs=model(inputs)
+        loss=criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+    total_loss = running_loss // len(test_loader)
+    total_acc = running_corrects.double() // len(test_loader)
+    
+    logger.info(f"Testing Loss: {total_loss}")
+    logger.info(f"Testing Accuracy: {total_acc}")
+
+def train(model, train_loader, validation_loader, criterion, optimizer , eps, hook):
+    epochs=eps
+    best_loss=1e6
+    image_dataset={'train':train_loader, 'valid':validation_loader}
+    loss_counter=0
+    
+    for epoch in range(epochs):
+        logger.info(f"Epoch: {epoch}")
+        for phase in ['train', 'valid']:
+            if phase=='train':
+                model.train()
+                hook.set_mode(smd.modes.TRAIN)
+            else:
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in image_dataset[phase]:
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase=='train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss // len(image_dataset[phase])
+            epoch_acc = running_corrects // len(image_dataset[phase])
+            
+            if phase=='valid':
+                if epoch_loss<best_loss:
+                    best_loss=epoch_loss
+                else:
+                    loss_counter+=1
+
+
+            logger.info('{} loss: {:.4f}, acc: {:.4f}, best loss: {:.4f}'.format(phase,
+                                                                                 epoch_loss,
+                                                                                 epoch_acc,
+                                                                                 best_loss))
+        if loss_counter==1:
+            break
+        if epoch==0:
+            break
+    return model
+    
+def net():
+    model = models.resnet50(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    model.fc = nn.Sequential(
+                   nn.Linear(2048, 128),
+                   nn.ReLU(inplace=True),
+                   nn.Linear(128, 5))
+    return model
+
+def create_data_loaders(data, batch_size):
+
+
+    
+
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        ])
+
+
+    dataset = datasets.ImageFolder(data, transform=test_transform)
+    lengths = [5221, 2611, 2609]
+    train_set, test_set , valid_set = torch.utils.data.random_split(dataset, lengths)
+    
+    
+    trainDataloader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True, num_workers=2)
+    
+    testDataloader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=False, num_workers=2)
+    
+    validDataloader = torch.utils.data.DataLoader(valid_set, batch_size=32, shuffle=False, num_workers=2)
+    
+    
+    
+    return trainDataloader, testDataloader , validDataloader
+
+def main(args):
+    logger.info(f'Hyperparameters are LR: {args.learning_rate}, Batch Size: {args.batch_size}')
+    logger.info(f'Data Paths: {args.data}')
+    
+    train_loader, test_loader, validation_loader=create_data_loaders(args.data, args.batch_size)
+    model=net()
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
+    
+    criterion = nn.CrossEntropyLoss(ignore_index=133)
+    eps = args.epochs
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.learning_rate)
+    
+    logger.info("Starting Model Training")
+    model=train(model, train_loader, validation_loader, criterion, optimizer , eps , hook)
+    
+    logger.info("Testing Model")
+    test(model, test_loader, criterion , hook)
+    
+    logger.info("Saving Model")
+    torch.save(model.cpu().state_dict(), os.path.join(args.model_dir, "model.pth"))
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--learning_rate', type=float)
+    parser.add_argument('--batch_size', type=int)
+    parser.add_argument('--epochs', type=int)
+    parser.add_argument('--data', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--model_dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--output_dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    
+    args=parser.parse_args()
+    print(args)
+    
+    main(args)
